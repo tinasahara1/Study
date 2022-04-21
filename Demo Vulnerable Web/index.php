82837aab101a69cf1e6eb7d065d5b5659e6839f7
zev <57553555+tinasahara1@users.noreply.github.com>
2022-04-21 16:14:38 +0700

    Add files via upload

---
+++ Demo Vulnerable Web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File include</title>
+</head>
+<style>
+    body {
+        background-color: LightGray;
+    }
+
+    .tab {
+    display: inline-block;
+    margin-left: 40px;
+}
+</style>
+
+<body>
+    <h1><center>Welcome to my website</center></h1> 
+
+    <a href="http://localhost:8080/weblfi/file1.php?file=Home" title="Home">Home</a>
+</body>
+</html>
